35fd790478c339fcaebbb52663a5e259cb788d2d
Kee Jefferys <Kee@loki.network>
2020-10-10 17:25:33 +1100

    Test Use of BLS lib

--- test.js
+++ test.js
@@ -1,16 +1,22 @@
-function generatePubKey(privKey){
-    var bls = require('noble-bls12-381');
-    var pubKey = bls.getPublicKey(privKey);
-    return pubKey
-  };
+var bls = require('noble-bls12-381');
 
-function testfunction2(){
-var arithmetic = require('arithmetic');
-var test = arithmetic.add(2, 4);
-return test
+function generatePubKey(privKey) {
+  var pubKey = bls.getPublicKey(privKey);
+  return pubKey
+};
+
+function signMessage(privKey, message) {
+  signature = bls.sign(message,privKey)
+  return signature
+};
+
+async function checkSig (signature, publicKey, message){
+let sig = await signature
+let isCorrect = await bls.verify(sig,message, publicKey)
+return isCorrect
 }
 
-module.exports = {generatePubKey: generatePubKey, myFunc2: testfunction2};
+module.exports = { generatePubKey: generatePubKey, signMessage: signMessage, checkSig: checkSig };
 
 
 //  browserify test.js --standalone myBundle > bundle.js
