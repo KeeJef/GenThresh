ba2ae1fc3cda3cc3ba56c4c70c139d8744c2c3d7
Kee Jefferys <Kee@loki.network>
2020-10-22 23:46:18 +1100

    working rooms

--- ServerSide/server.js
+++ ServerSide/server.js
@@ -1,22 +1,30 @@
-    io = require("socket.io"),
-    server = io.listen(8000);
+io = require("socket.io"),
+   server = io.listen(8000);
 
 users = [];
-server.on('connection', function(socket) {
+server.on('connection', function (socket) {
    console.log('A user connected');
-   socket.on('setUsername', function(data) {
+
+   socket.on('setUsername', function (data) {
       console.log(data);
-      
-      if(users.indexOf(data) > -1) {
+
+      if (users.indexOf(data) > -1) {
          socket.emit('userExists', data + ' username is taken! Try some other username.');
       } else {
          users.push(data);
-         socket.emit('userSet', {username: data});
+         socket.emit('userSet', { username: data });
       }
    });
-   
-   socket.on('msg', function(data) {
+
+   socket.on('create', function (room) {
+      console.log("a new room was created with the name" + room)
+      socket.join(room);
+      
+   });
+
+   socket.on('msg', function (data) {
       //Send message to everyone
-      server.sockets.emit('newmsg', data);
+      server.sockets.in(data.roomid).emit('newmsg', data);
    })
 });
+
