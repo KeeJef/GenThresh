73cedf32a2eea409490dbd9fcbd2021d01d845e3
Kee Jefferys <Kee@loki.network>
2020-11-01 14:04:04 +1100

    Small update

--- test.js
+++ test.js
@@ -1,5 +1,26 @@
 var bls = require('noble-bls12-381');
 
+testfunction()
+
+async function testfunction() {
+
+  privKey = "970a3e0249bc5e464291d5eee88ae364d8b794f648950055a382a28ed73d655472f218dae342d3ee4566443ff23a78d85580eb4b11954f6286c6cfc67ad1c082"
+  message1 = "test"
+  message2 = "tes3"
+
+  let signature1 = await bls.sign(message1, privKey)
+  let signature2 = await bls.sign(message2, privKey)
+
+  console.log(signature1 +"     "+ signature2)
+
+  signature1 = bufferToHex(signature1)
+  signature2 = bufferToHex(signature2)
+
+  if (signature1 == signature2){
+    console.log ("Sigs are same")
+  }
+};
+
 function generatePubKey(privKey) {
   var pubKey = bls.getPublicKey(privKey);
   return pubKey
