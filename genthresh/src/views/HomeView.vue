eac03a505441ee24c23d85876d438669b7dccafa
Kee Jefferys <kee@loki.network>
2022-09-07 23:12:59 +1000

    Basic stub project

---
+++ genthresh/src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <div class="flex justify-center"><img alt="Vue logo" src="../assets/logo.png"></div>
+    
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
