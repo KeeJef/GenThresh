904b2195942dc6ef9e4a5b054c2a7d50dec0415f
Kee Jefferys <kee@loki.network>
2022-09-24 18:19:55 +1000

    Finish core verify

--- genthresh/src/views/VerifyView.vue
+++ genthresh/src/views/VerifyView.vue
@@ -3,15 +3,15 @@
   <div class="flex flex-wrap flex-row justify-center gap-1 pb-5 mx-10">
     <label for="files" class="select-none transition-colors duration-500 ease-in-out bg-purple-400 rounded-md p-3 text-white font-sans font-semibold text-3xl shadow-xl hover:bg-purple-600">⬆️💾 Import Key</label>
       <input @change="processKey" id="files" class="hidden" type="file">
+    <mainButton @click="validate" title="👀 Verify" />
     <mainButton title=" 👨‍👩‍👧‍👦 Verify Group" />
-    <mainButton title="👀 Verify" />
   </div>
 
   <div class="flex justify-center text-4xl pb-2">Public Key</div>
 
   <div class="flex justify-center mb-4">
     <EditableArea
-      v-model="importedKey"
+      v-model="pubKey"
       :noHTML="false"
       class="w-3/5 break-words border-2 rounded-xl border-yellow-800 text-2xl p-8"
     ></EditableArea>
@@ -36,6 +36,10 @@
       class="w-3/5 break-words border-2 rounded-xl border-yellow-800 text-2xl p-8"
     ></EditableArea>
   </div>
+ <div v-if="signatureChecked">
+  <div v-if="isValid" class="flex justify-center text-4xl pb-2">Valid Signature✔️</div>
+  <div v-if="!isValid" class="flex justify-center text-4xl pb-2">Invalid Signature/Data ❌</div>
+  </div>
 </template>
 
 <script>
@@ -45,18 +49,18 @@ import { defineComponent } from "vue";
 import TitleCard from "@/components/TitleCard.vue";
 import mainButton from "@/components/mainButton.vue";
 import EditableArea from "@/components/EditableArea.vue";
+import helpers from '@/helperFunctions/helperFunctions.js'
 //import TextDisplay from "@/components/TextDisplay.vue";
 
 export default defineComponent({
   name: "VerifyView",
   data() {
     return {
-      keysImported:false,
-      signed:false,
+      signatureChecked:false,
+      isValid: false,
       message:"",
-      importedKey:"",
       signature:"",
-      privKey:""
+      pubKey:""
     };
   },
 
@@ -76,11 +80,33 @@ export default defineComponent({
       // here we tell the reader what to do when it's done reading...
       reader.onload = (readerEvent) => {
         var rawFileData = readerEvent.target.result; // this is the content!
-        this.importedKey = rawFileData
 
+        try {
+          var keys = rawFileData.split('\n')
+          var publicKey = keys[1]
+          if (publicKey == undefined) {
+            throw "Undefined Key"
+          }
+          this.pubKey = publicKey
+          } catch (error) {
+            window.alert("Key improperly formatted")
+          }
         //split off public key for verification
       };
     },
+    async validate(){
+      if (this.message && this.signature && this.pubKey) {
+        try {
+          this.isValid = await helpers.checkSig(this.signature,this.pubKey,this.message)
+        } catch (error) {
+          this.isValid = false
+        }
+        this.signatureChecked = true
+      }
+      else{
+        window.alert("Missing signature, public key or message")
+      }
+    }
   }
 });
 </script>
