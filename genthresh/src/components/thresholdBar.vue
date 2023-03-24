ce142a404b3b312b1aec71eb65818320915ccacd
Kee Jefferys <kee@loki.network>
2023-03-24 20:12:34 +1100

    Better threshold selector

--- genthresh/src/components/thresholdBar.vue
+++ genthresh/src/components/thresholdBar.vue
@@ -1,21 +1,50 @@
 <template>
+  <div class="relative">
+    <div style="width: 100%" class="absolute bg-yellow-400 h-9"></div>
+
     <div class="relative">
-        <div style="width:100%" class="absolute bg-yellow-400 h-9"></div>
-        <div :style="{width:this.maxNumber+'%'}" class="absolute bg-purple-400 h-9"></div>
-        <div :style="{width:this.threshold+'%'}" class="absolute bg-purple-600 h-9"></div>
+      <div
+        :style="{ width: this.maxNumber + '%' }"
+        class="absolute bg-purple-400 h-9"
+      >
+        <div class="relative shrink-0 flex justify-end bottom-12 left-5">
+          <div class="flex justify-center items-center relative rounded-md py-1.5 px-3 bg-purple-400">
+            {{ maxNumber }}
+            <div
+              class="absolute w-3 h-3 bg-purple-400 transform rotate-45 -bottom-1 right-1/2 translate-x-1/2"
+            ></div>
+          </div>
+        </div>
+      </div>
     </div>
+
+    <div class="relative">
+      <div
+        :style="{ width: this.threshold + '%' }"
+        class="absolute bg-purple-600 h-9"
+      >
+      <div class="relative flex justify-end bottom-12 left-5">
+          <div class="flex relative rounded-md py-1.5 px-3 bg-purple-600">
+            {{ threshold }}
+            <div
+              class="absolute w-3 h-3 bg-purple-600 transform rotate-45 -bottom-1 right-1/2 translate-x-1/2"
+            ></div>
+          </div>
+        </div>
+    </div>
+    </div>
+
+  </div>
 </template>
-  
-  <script>
-  export default {
-    name: "ThresholdBar",
-   
-    data: () => ({
-    }),
-    props: {
-      maxNumber: String,
-      threshold: String,
-    },
-  };
-  </script>
-  
+
+<script>
+export default {
+  name: "ThresholdBar",
+
+  data: () => ({}),
+  props: {
+    maxNumber: String,
+    threshold: String,
+  },
+};
+</script>
--- genthresh/src/views/CreateGroupView.vue
+++ genthresh/src/views/CreateGroupView.vue
@@ -1,20 +1,42 @@
 <template>
   <TitleCard title="Create Group" />
-  <div class="flex justify-center h-10 m-5">
+  <div class="flex justify-center h-10 my-5">
     <thresholdBar
       :threshold="this.threshold"
       :maxNumber="this.maxNumber"
       class="w-3/5"
     ></thresholdBar>
   </div>
-  <div class="flex flex-col justify-center">
-    <div class="flex justify-center text-2xl pb-2">Number of Signers</div>
-    <input type="range" min="1" max="100" class="slider" v-model="maxNumber" />
+  <div class="flex flex-col justify-center pb-6">
+    <div class="flex justify-center text-2xl pb-3 select-none">
+      Number of Signers : {{ maxNumber }}
+    </div>
+    <div class="flex justify-center">
+      <input
+        type="range"
+        min="1"
+        max="100"
+        class="slider w-3/5"
+        style="--thumb-color: #c084fc"
+        v-model="maxNumber"
+      />
+    </div>
   </div>
 
   <div class="flex flex-col justify-center">
-    <div class="flex justify-center text-2xl pb-2">Threshold</div>
-    <input type="range" min="1" max="100" class="slider" v-model="threshold" />
+    <div class="flex justify-center text-2xl pb-3 select-none">
+      Threshold : {{ threshold }}
+    </div>
+    <div class="flex justify-center">
+      <input
+        type="range"
+        min="1"
+        max="100"
+        class="slider w-3/5"
+        style="--thumb-color: #9333ea"
+        v-model="threshold"
+      />
+    </div>
   </div>
 </template>
 
@@ -28,8 +50,8 @@ import ThresholdBar from "@/components/thresholdBar";
 export default defineComponent({
   name: "CreateGroupView",
   data: () => ({
-    threshold: 3,
-    maxNumber: 10,
+    threshold: 10,
+    maxNumber: 30,
   }),
 
   components: {
@@ -38,3 +60,34 @@ export default defineComponent({
   },
 });
 </script>
+
+<style>
+.slider {
+  -webkit-appearance: none;
+  height: 15px;
+  
+  background: #e4e3e3;
+  outline: none;
+  opacity: 1;
+  -webkit-transition: 0.2s;
+  transition: opacity 0.2s;
+}
+
+.slider::-webkit-slider-thumb {
+  -webkit-appearance: none;
+  appearance: none;
+  width: 25px;
+  height: 25px;
+  border-radius: 50%;
+  background: var(--thumb-color, #9333ea);
+  cursor: pointer;
+}
+
+.slider::-moz-range-thumb {
+  width: 25px;
+  height: 25px;
+  border-radius: 50%;
+  background: var(--thumb-color, #9333ea);
+  cursor: pointer;
+}
+</style>
